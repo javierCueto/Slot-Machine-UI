9287d587cdf070b025ff88daeb58343eb42c28fe
Javier <javelie@outlook.com>
2020-05-24 13:54:02 -0600

    Initial Commit

---
+++ Slot Machine UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine UI
+//
+//  Created by José Javier Cueto Mejía on 24/05/20.
+//  Copyright © 2020 Pozolx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
